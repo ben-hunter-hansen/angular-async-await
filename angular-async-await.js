778d7ccc6c6b30742fc9ecb40d66b8041d405dd0
ben-hunter-hansen <ben.hunter.hansen@gmail.com>
2016-02-16 23:27:41 -0600

    added src directory

--- angular-async-await.js
+++ angular-async-await.js
@@ -1,40 +1,72 @@
-/**
- The MIT License (MIT)
+"use strict";
 
- Copyright (c) 2016 Benjamin Hunter Hansen
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+var _regenerator = require("babel-runtime/regenerator");
 
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
+var _regenerator2 = _interopRequireDefault(_regenerator);
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- */
+var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
 
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
-import "babel-polyfill";
+require("babel-polyfill");
 
-let $async = ['$rootScope', ($rootScope) => {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var $async = ['$rootScope', function ($rootScope) {
   "use strict";
 
-  return cb => {
-    return async function (...args) {
-      await cb(...args);
-      $rootScope.$apply();
-    }
-  };
-}];
+  return function (cb) {
+    return (function () {
+      var ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee() {
+        var _args = arguments;
+        return _regenerator2.default.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return cb.apply(undefined, _args);
 
-export default angular.module('angular.async', [])
-  .factory('$async', $async)
+              case 2:
+                $rootScope.$apply();
+
+              case 3:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+      return function (_x) {
+        return ref.apply(this, arguments);
+      };
+    })();
+  };
+}]; /**
+     The MIT License (MIT)
+    
+     Copyright (c) 2016 Benjamin Hunter Hansen
+    
+     Permission is hereby granted, free of charge, to any person obtaining a copy
+     of this software and associated documentation files (the "Software"), to deal
+     in the Software without restriction, including without limitation the rights
+     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     copies of the Software, and to permit persons to whom the Software is
+     furnished to do so, subject to the following conditions:
+    
+     The above copyright notice and this permission notice shall be included in all
+     copies or substantial portions of the Software.
+    
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+     SOFTWARE.
+     */
+
+exports.default = angular.module('angular.async', []).factory('$async', $async);
